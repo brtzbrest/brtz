2bef08ec4626c506a505c81c66792aef015394f8
brtzbrest <edu.brest@yandex.ru>
2016-10-11 08:58:14 +0300

    rs task 1

---
+++ rs_1.js
@@ -0,0 +1,35 @@
+/* Returns the result of concatenation of two strings.
+ function concatinateString(value1,value2) {
+ var rez = value1 + value2;
+ alert('\'' + rez + '\'');
+ }
+ concatinateString('aa','bb');
+ concatinateString('aa','');
+ concatinateString('','bb');
+
+ */
+/* Returns the length of given string.
+ function getStringLength(value) {
+ alert(value.length);
+ }
+ getStringLength('aaaaa');
+ getStringLength('b');
+ getStringLength('');
+ */
+
+/* Returns the result of string template and given parameters firstName and lastName.
+ function getStringFromTemplate(firstName, lastName) {
+ alert('\''+'Hello' + ',' + ' ' + firstName + ' ' + lastName + '!' + '\'' )
+ }
+ getStringFromTemplate ('John', 'Doe');
+ getStringFromTemplate ('Chuck', 'Norris');
+ */
+/* Returns a first char of the given string.
+ function getFirstChar(value){
+ alert('\''+ value.charAt(0) + '\'');
+ }
+ getFirstChar ('John');
+ getFirstChar ('cat');
+ */
+
+/* Removes a leading and trailing whitespace characters from string. удалить начальные и конечные пробелы из строки*/
